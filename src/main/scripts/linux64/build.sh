8517835613cfe007809c8b1cc0c4d659e9bc154b
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-07-29 08:44:56 +0200

    Update build.sh

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -29,6 +29,8 @@ cmake ../../c++ \
 -DOGRE_INSTALL_PDB=FALSE \
 -DFREETYPE_LIBRARY="$PWD/ogredeps/linux64/lib/libfreetype.a" \
 -DFREETYPE_INCLUDE_DIRS="$PWD/ogredeps/linux64/include" \
+-DFreeImage_INCLUDE_DIR="$PWD/freeimage/linux64/include" \
+-DFreeImage_LIBRARY="test/libFreeImage.a" \
 -DZLIB_LIBRARY="$PWD/ogredeps/linux64/lib/libzlib.a" \
 -DZLIB_INCLUDE_DIR="$PWD/ogredeps/linux64/include" \
 -DZZip_INCLUDE_DIRS="$PWD/ogredeps/linux64/include" \
